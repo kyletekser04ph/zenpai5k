494f12b9ef0372ae2b9872c2842256fd5a20f0ac
BishwoBastola03 <161615824+BishwoBastola03@users.noreply.github.com>
2024-04-18 22:09:22 +0545

    Update rbg.js

--- scripts/cmds/rbg.js
+++ scripts/cmds/rbg.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 
-const apiKey = "Qfmz1xcUvaehs1SaYFLzsBzp";
+const apiKey = "MpWrTb2MH8ykq25wWhYgxQhk";
 
 module.exports = {
     config: {
