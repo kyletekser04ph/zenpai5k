6a25a78a78e107b872bb1dcf35a146caf104a83b
Kyutashii <kyutashiixdd@gmail.com>
2024-06-27 12:37:08 +0800

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -23,9 +23,9 @@ module.exports = {
 			session2: "noon",
 			session3: "afternoon",
 			session4: "evening",
-			leaveType1: "left",
-			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userName} {type} the group"
+			leaveType1: "𝗹𝗲𝗳𝘁",
+			leaveType2: "𝘄𝗮𝘀 𝗸𝗶𝗰𝗸𝗲𝗱 𝗳𝗿𝗼𝗺",
+			defaultLeaveMessage: "𝗴𝗼𝗼𝗱 𝗯𝘆𝗲 𝗭𝗲𝗻𝗽𝗮𝗶 {userNameTag}.\n\n📜𝗿𝗲𝗮𝘀𝗼𝗻: {type} 𝘁𝗵𝗲 𝗴𝗿𝗼𝘂𝗽\n\nnagleft tama yan para ma-backstab ka namin pangit ka naman napilitan lang mga magulang mo palakihin ka, tamang tamang nagleft HAHAHAHA may matotopic kami sa gc gawin naming memes op mo hanapin namin ra mo walang dulot umalis display ka lang naman kala mo kung sino dapat sayo sinasako walang ambag sa gc namin layas dukha."
 		}
 	},
 
